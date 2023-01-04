a0e8a29c024d67a95f5ca374875ff17324ad9a6b
Humbertov374 <humbertov374@gmail.com>
2023-01-03 19:43:30 -0700

    sample

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,62 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-`;
-}
-
-module.exports = generateMarkdown;
+    let copywrite = '';
+    const mit = `Copywrite (c) ${data.year} ${data.fullName}
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`;
+    const apache = `Copyright ${data.year} ${data.fullName}
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License`;
+    const gnu = `Copyright (C) ${data.year}  ${data.fullName}
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see https://www.gnu.org/licenses.`;
+  
+    if (data.license == 'Apache2.0') {
+      copywrite = apache;
+    }
+    else if (data.license == 'GNUv3') {
+      copywrite = gnu;
+    }
+    else {
+      copywrite = mit;
+    }
+  
+    return `# ${data.title}
+    ## Description
+    ![Badge](https://img.shields.io/badge/license-${data.license}-green)<br>
+    ${data.description}
+    ## Table of Contents
+    * [Installation](#Installation)
+    * [Usage](#Usage)
+    * [Contributing](#Contributing)
+    * [Tests](#Tests)
+    * [License](#License)
+    * [Questions](#Questions)
+    ## Installation
+    ${data.installation}
+    ## Usage
+    ${data.usage}
+    ## Contributing
+    ${data.contributing}
+    ## Tests
+    ${data.tests}
+    ## License
+    ${copywrite}
+    ## Questions
+    Email: ${data.email}<br>
+    Git Hub: https://www.github.com/${data.username}
+  `;
+  }
+  
+  module.exports = generateMarkdown;
---
+++ sample-README.md
@@ -0,0 +1,31 @@
+# Sample
+  ## Description
+  ![Badge](https://img.shields.io/badge/license-GNUv3.0-green)<br>
+  This is a sample of how the README Generator works.<br>
+  Video Demo: https://youtu.be/gMVBGiAhhis
+  ## Table of Contents
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [Contributing](#Contributing)
+  * [Tests](#Tests)
+  * [License](#License)
+  * [Questions](#Questions)
+  ## Installation
+  Download files from repo and use npm install.
+  ## Usage
+  It generates professional README's for your project.
+  ## Contributing
+  Fork the repo and submit a pull request.
+  ## Tests
+  No tests to run.
+  ## License
+  Copywrite (c) 2021 Trevor Smith
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+  
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  ## Questions
+  Email: sample@gmail.com<br>
+  Git Hub: https://www.github.com/trevorsmithbanjo
